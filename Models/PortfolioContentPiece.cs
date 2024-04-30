d39a2a2096658e54f85dea1f8c71967a9d154c0d
Eevi Karjalainen <Eevi.Karjalainen@metropolia.fi>
2024-04-30 15:37:10 +0300

    Added Portfolio Content

---
+++ Models/PortfolioContentPiece.cs
@@ -0,0 +1,14 @@
+﻿namespace EevikaPortfolioProject.Models
+{
+    public class PortfolioContentPiece
+    {
+        public int WorkId { get; set; }
+        public string ContentTitle { get; set; } = string.Empty;
+
+        public string ContentType { get; set;} = string.Empty;
+
+        public string ContentReference { get; set; } = string.Empty;
+
+        public string ContentText { get; set; } = string.Empty;
+    }
+}
---
+++ Models/PortfolioWork.cs
@@ -0,0 +1,12 @@
+namespace EevikaPortfolioProject.Models
+{
+    public class PortfolioWork
+    {
+        public int Id { get; set; }
+
+        public string PageName { get; set; } = string.Empty;
+
+        public string ButtonIconStyle { get; set; } = "";
+
+    }
+}
